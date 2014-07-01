a2711f1b38c0000f274ada864ab85bb6802fa973
Mohammad Razeghi <razeghi71@gmail.com>
2014-07-02 01:07:12 +0430

    set model files encoding to utf8

--- Sekhavat/helpdesk/models.py
+++ Sekhavat/helpdesk/models.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from mainapp.models import Helper, Helpee
 
 
 class HelpTask(models.Model):
-    helper = models.ForeignKey('Helper')
-    helpee = models.ForeignKey('Helpee')
+    helper = models.ForeignKey(Helper)
+    helpee = models.ForeignKey(Helpee)
     help_price = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
 
