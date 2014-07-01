bdc7f2bf02e08e15b89662b073ae389c4c4f5368
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2014-07-01 05:23:56 +0430

    Get the fucking bootstrap shit to actually work

--- Sekhavat/Sekhavat/settings.py
+++ Sekhavat/Sekhavat/settings.py
@@ -24,6 +24,10 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'Sekhavat/templates'),
+)
+
 ALLOWED_HOSTS = []
 
 
@@ -84,3 +88,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "Sekhavat/static"),
+)
--- Sekhavat/Sekhavat/urls.py
+++ Sekhavat/Sekhavat/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +16,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^home$', TemplateView.as_view(template_name='layout.html'))
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
