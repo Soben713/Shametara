3189bae31ffbd1eb75507262d79a8b4caabdcd78
Mohammad Razeghi <razeghi71@gmail.com>
2014-07-01 14:23:36 +0430

    remove library files

--- Khadem/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/HelloWorld.ap_ \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes.dex \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/CordovaLib/ant-build/classes.jar \
--- Khadem/platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes.dex : \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/HelloWorld.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
-/home/home/mohammad/Desktop/NovinTahlil/Khadem/platforms/android/ant-build/dexedLibs/classes-62aeef763aee823cb7253933804cbb64.jar \
