c230ba1035a0606744d72c0345efac0b7db23514
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2014-07-30 23:56:11 +0430

    added add_company.html

--- Sekhavat/mainapp/urls.py
+++ Sekhavat/mainapp/urls.py
@@ -8,5 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^login$', TemplateView.as_view(template_name='login.html'), name="login"),
+    url(r'^addCompany$', TemplateView.as_view(template_name='add_company.html'), name="addCompany"),
     url(r'^$', TemplateView.as_view(template_name='main.html'))
+
 )
