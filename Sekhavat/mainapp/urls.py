5f4a9cbd2333da24797cd69bec008929ed7864e5
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2014-08-01 13:13:14 +0430

    login pages ui, server side

--- Sekhavat/mainapp/urls.py
+++ Sekhavat/mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from mainapp.views import add_manager,add_helper,add_operator,login
 
 from django.contrib import admin
 from django.views.generic.base import TemplateView
@@ -7,8 +8,15 @@ from django.views.generic.base import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^login$', TemplateView.as_view(template_name='login.html'), name="login"),
     url(r'^addCompany$', TemplateView.as_view(template_name='add_company.html'), name="addCompany"),
+    url(r'^addOperator$', TemplateView.as_view(template_name='add_operator.html'), name="addOperator"),
+    url(r'^addHelper$', TemplateView.as_view(template_name='add_helper.html'),name="addHelper"),
+    url(r'^editInformation', TemplateView.as_view(template_name='edit_information.html'),name="editInformation"),
+    url(r'^login$', TemplateView.as_view(template_name='login.html'),name="login"),
+    url(r'^addcompany$', add_manager),
+    url(r'^addoperator$', add_operator),
+    url(r'^addhelper$', add_helper),
+    url(r'^doLogin$', login),
     url(r'^$', TemplateView.as_view(template_name='main.html'))
 
 )
--- Sekhavat/mainapp/views.py
+++ Sekhavat/mainapp/views.py
@@ -1,15 +1,72 @@
-from django.shortcuts import redirect
 from django.http import HttpResponse
+from mainapp.models import CompanyManager, Company, Operator, Helper
+from django.shortcuts import redirect
+from django.contrib import auth
 
+def add_manager(request):
+    company = Company()
+    company.curPrice = 0
+    company.name = request.POST['companyName']
+    company.save()
+
+    manager = CompanyManager.objects.create_user(username=request.POST['userName'],
+                                                 password=request.POST['password'],company=company)
+    manager.name = request.POST['managerName']
+    manager.family = request.POST['managerFamily']
+    manager.phone = request.POST['phoneNumber']
+
+
+    manager.company = company
+    manager.save()
+    return HttpResponse()
+
+def add_operator(request):
+    manager = CompanyManager.objects.get(username=request.user.username)
+    operator = Operator.objects.create_user(username=request.POST['userName'],
+                                            password=request.POST['password'],company=manager.company)
+    operator.name = request.POST['operatorName']
+    operator.family = request.POST['operatorFamily']
+    operator.phone = request.POST['phoneNumber']
+    operator.save()
+    return HttpResponse()
+
+
+
+def add_helper(request):
+    helper = Helper()
+    helper.name = request.POST['helperName']
+    helper.family = request.POST['helperFamily']
+    helper.phone = request.POST['phoneNumber']
+    helper.username = request.POST['userName']
+    helper.password = request.POST['password']
+    helper.save()
+    return HttpResponse()
 
 def login(request):
-    username = request.POST['username']
+    username=request.POST['username']
+    passw=request.POST['password']
+    exists=True
+    user = auth.authenticate(username=username, password=passw)
+    try:
+        manager =CompanyManager.objects.get(username=username)
+    except CompanyManager.DoesNotExist:
+        manager = None
+        try:
+            operator = Operator.objects.get(username=username)
+        except Operator.DoesNotExist:
+            operator = None
+    if user is not None and username == "admin":
+        auth.login(request, user)
+        return HttpResponse("admin")
 
-    if username == "admin":
-        return HttpResponse("/managers/shametara")
+    if user is not None and manager is not None:
+        auth.login(request, user)
+        return HttpResponse("manager")
+    else:
+        if user is not None and operator is not None:
+            auth.login(request, user)
+            return HttpResponse("operator")
+    #message the user to re enter username and password
+    return HttpResponse("None")
 
-    if username == "company":
-        return HttpResponse("/managers/company")
 
-    if username == "operator":
-        return HttpResponse("/helpdesk/")
